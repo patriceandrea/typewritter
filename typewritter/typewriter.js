2ee99ac516c9befebe535bff774187819a36e8cc
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-15 16:43:27 +0000

    add typewritter.js

--- typewritter/typewriter.js
+++ typewritter/typewriter.js
@@ -2,9 +2,10 @@ const sentence = "hello there from lighthouse labs\n";
 
 for (const char of sentence) {
 
-  // process.stdout.write(char);
+
   setTimeout(() => {
     // print the char here
-    console.log(char)
+    // console.log(char)
+    process.stdout.write(char);
   }, 50) // <= 1s delay to make it noticeable. Can dial it down later.
 } 
